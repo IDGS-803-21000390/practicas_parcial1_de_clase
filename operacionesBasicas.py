d082dc6f03f9ab0db9a171034256a8b11ce7f4de
IDGS-803-21000390 <156369037+IDGS-803-21000390@users.noreply.github.com>
2024-01-26 19:55:10 -0600

    YPH 26/01/2023 CINEPOLIS

--- operacionesBasicas.py
+++ operacionesBasicas.py
@@ -6,6 +6,35 @@ app=Flask(__name__)
 def formulario():
     return render_template("formulario.html")
 
+@app.route("/cine")
+def cine():
+    return render_template("cine.html")
+
+@app.route("/respuestas",methods=["GET","POST"])
+def r():
+    if request.method=="POST":
+        n=request.form.get("nombre")
+        CB=int(request.form.get("cantidadBoletos"))
+        TC=request.form.get("tarjetaCine")
+        TP=CB*12
+        TPD=0
+        p=int(request.form.get("cantidadCompradores"))
+        limiteB=7
+        limiteBT=limiteB*p
+       
+        if CB<= limiteBT:
+            if CB >5:
+                TPD=TP-(TP*0.15)
+            elif CB <= 5 and CB >= 3:
+                 TPD=TP-(TP*0.10)
+            else:
+                TPD=TP    
+            if TC == "si":
+                TPD=TPD-(TPD*0.10)
+            return  render_template('cine.html', valor_a_pagar=TPD,nombre=n,cantidad=CB)
+        else:
+            return  render_template('cine.html', alerta="No puedes comprar {}, solo {}".format(CB,limiteBT))
+
 
 @app.route("/resultado",methods=["GET","POST"])
 def resultado():
